3ae7d84bee3e4fefc02565e187ada0978932fc0f
Asher <gilaniasher@gmail.com>
2020-06-22 21:48:22 -0400

    Added back button banner to KitConfirmation and ConstructionInstructions

--- ConstructorClient/src/screens/construction/ConstructionInstructions.js
+++ ConstructorClient/src/screens/construction/ConstructionInstructions.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-export default function ConstructionInstructions() {
+export default function ConstructionInstructions(props) {
     return (
-        <View>
-            <Text>Construction Instructions</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.welcome}>
+                <TouchableOpacity onPress={() => props.navigation.goBack()} style={styles.backButton}>
+                    <Icon name='ios-arrow-back' size={28} color='white' />
+                </TouchableOpacity>
+                <Text style={styles.headerText}>Construction Instructions</Text>
+            </View>
+
+            <View style={styles.bottomContainer}>
+                <Text>Hello</Text>
+            </View>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    welcome: {
+        flex: 1,
+        backgroundColor: '#003366',
+        padding: 20,
+        paddingBottom: 0,
+        flexDirection: 'row'
+    },
+    backButton: {
+        paddingTop: '1%',
+        paddingRight: '5%'
+    },
+    headerText: {
+        color: 'white',
+        fontSize: 25,
+        fontWeight: 'bold'
+    },
+    bottomContainer: {
+        flex: 9.5,
+        padding: 20
+    }
 });
