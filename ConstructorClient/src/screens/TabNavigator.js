5b11475671478ef1014cbc2c2b42ceaa0b20b67c
Asher <gilaniasher@gmail.com>
2020-07-04 15:39:43 -0400

    Does not use listener for custom backend to get UID
    1. After creating Firebase User, calls customSignup using a .then parameter
    2. PROBLEM For some reason, The Firebase/Custom signup stuff gets called multiple times. Must be async problems
    3. PROBLEM Need to fix navigation containers so you can't click back arrow to get back to login/signup screens

--- ConstructorClient/src/screens/TabNavigator.js
+++ ConstructorClient/src/screens/TabNavigator.js
@@ -28,16 +28,16 @@ const routeIcons = ({ route }) => ({
 const Tab = createMaterialBottomTabNavigator();
 
 export default function MyTabs() {
-  return (
-    <Tab.Navigator
-        initialRouteName="Dashboard"
-        barStyle={{ backgroundColor: '#003366'}}
-        screenOptions={routeIcons}
-    >
-        <Tab.Screen name="Dashboard" component={DashboardRoutes} />
-        <Tab.Screen name="Timeline" component={TimelineScreen} />
-        <Tab.Screen name="Construction" component={ConstructionRoutes} />
-        <Tab.Screen name="Profile" component={Profile} />
-    </Tab.Navigator>
-  );
+    return (
+        <Tab.Navigator
+            initialRouteName="Dashboard"
+            barStyle={{ backgroundColor: '#003366'}}
+            screenOptions={routeIcons}
+        >
+            <Tab.Screen name="Dashboard" component={DashboardRoutes} />
+            <Tab.Screen name="Timeline" component={TimelineScreen} />
+            <Tab.Screen name="Construction" component={ConstructionRoutes} />
+            <Tab.Screen name="Profile" component={Profile} />
+        </Tab.Navigator>
+    );
 }
