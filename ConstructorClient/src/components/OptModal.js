f56b3b71fedd40ff05cb148b60dc499cd522716d
Asher <gilaniasher@gmail.com>
2020-07-11 19:55:23 -0400

    Designed opt in modal screens
    1. Added a number picker library for picking number of kits
    2. Added back button to go back to initial modal screen
    3. [TODO] Add functionality for kit signup button

--- ConstructorClient/src/components/OptModal.js
+++ ConstructorClient/src/components/OptModal.js
@@ -1,8 +1,27 @@
 import React, { useState } from 'react'
-import { View, Text, StyleSheet, Modal } from 'react-native'
+import { View, Text, StyleSheet, Modal, Dimensions, TouchableOpacity } from 'react-native'
+import NumericInput from 'react-native-numeric-input'
 import AwesomeButtonRick from 'react-native-really-awesome-button/src/themes/blue'
 
+const width = Dimensions.get('window').width
+const height = Dimensions.get('window').height
+
 export default function OptModal(props) {
+    const [formVisible, setFormVisible] = useState(false)
+    const [state, setState] = useState({
+        numKits: 0
+    })
+
+    const changeState = (id, val) => {
+        setState(prevState => ({
+            ...prevState,
+            [id]: val
+        }))
+    }
+
+    const signupForKits = () => {
+        console.log('Submit kit signup')
+    }
 
     return (
         <Modal
@@ -11,19 +30,56 @@ export default function OptModal(props) {
             visible={props.visible}
             onRequestClose={() => console.log('Modal has been closed')}
         >
-            <View style={styles.centeredView}>
-                <Text style={styles.modalHeader}>You have currently opted out from building</Text>
-                <AwesomeButtonRick
-                    type='anchor'
-                    onPress={() => console.log('Pulling up the opt in form')}
-                    borderRadius={15}
-                    stretch={true}
-                    backgroundColor={'#003366'}
-                    backgroundDarker={'#003366'}
-                >
-                    Opt In for Next Round
-                </AwesomeButtonRick>
-            </View>
+                {formVisible ?
+                    <View style={styles.formContainer}>
+                        <View style={styles.backContainer}>
+                            <TouchableOpacity onPress={() => setFormVisible(false)}>
+                                <Text style={styles.backButton}>X</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={{flex: 9.5, alignItems: 'center'}}>
+                            <Text style={styles.formText}>How many face shield kits do you want delivered?{'\n\n'}(1 kit = 10 shields)</Text>
+                            <NumericInput
+                                type='up-down'
+                                onChange={value => changeState('numKits', value)}
+                                rounded
+                                minValue={0}
+                                maxValue={30}
+                                editable={false}
+                                textColor='white'
+                                totalWidth={width * 0.7}
+                                totalHeight={height * 0.15}
+                            />
+                            {state.numKits != 0 &&
+                                <AwesomeButtonRick
+                                    type='anchor'
+                                    onPress={signupForKits}
+                                    borderRadius={15}
+                                    stretch={true}
+                                    backgroundColor={'#003366'}
+                                    backgroundDarker={'#003366'}
+                                    style={styles.kitButton}
+                                >
+                                    Sign Up for Kits
+                                </AwesomeButtonRick>
+                            }
+                        </View>
+                    </View>
+                    :
+                    <View style={styles.centeredView}>
+                        <Text style={styles.modalHeader}>You have currently opted out from building</Text>
+                        <AwesomeButtonRick
+                            type='anchor'
+                            onPress={() => setFormVisible(true)}
+                            borderRadius={15}
+                            stretch={true}
+                            backgroundColor={'#003366'}
+                            backgroundDarker={'#003366'}
+                        >
+                            Opt In for Next Round
+                        </AwesomeButtonRick>
+                    </View>
+                }
         </Modal>
     )
 }
@@ -35,6 +91,31 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: 'rgba(46, 49, 49, .98)',
         paddingHorizontal: '5%'
+    },
+    backContainer: {
+        flex: 1,
+        width: '100%',
+        alignItems: 'flex-start',
+        marginBottom: '18%'
+    },
+    backButton: {
+        color: 'white',
+        textAlign: 'left',
+        fontSize: 30
+    },
+    formContainer: {
+        flex: 1,
+        backgroundColor: 'rgba(46, 49, 49, .98)',
+        padding: '7%'
+    },
+    kitButton: {
+        marginTop: '20%'
+    },
+    formText: {
+        fontSize: 25,
+        color: 'white',
+        textAlign: 'center',
+        paddingBottom: '20%'
     },
     modalHeader: {
         color: 'white',
