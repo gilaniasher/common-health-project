503de6f6887136323b3a8f4033e53e2b3c27657e
Asher <gilaniasher@gmail.com>
2020-08-01 22:40:34 -0400

    Updates

---
+++ precache-manifest.f11853f2171144fbdab5add2cc3dd274.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9094497063d4ed0b7b6243bafddeecbe",
+    "url": "/common-health-project/index.html"
+  },
+  {
+    "revision": "fcf69fdf46445ae50053",
+    "url": "/common-health-project/static/css/main.610b2798.chunk.css"
+  },
+  {
+    "revision": "12b5036f9195fdc1cbd4",
+    "url": "/common-health-project/static/js/2.c97a5380.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/common-health-project/static/js/2.c97a5380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcf69fdf46445ae50053",
+    "url": "/common-health-project/static/js/main.5d699570.chunk.js"
+  },
+  {
+    "revision": "3ba0d6ae62ea5046b475",
+    "url": "/common-health-project/static/js/runtime-main.c09a9fc0.js"
+  }
+]);
